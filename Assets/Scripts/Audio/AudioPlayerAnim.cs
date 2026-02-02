4290b65ae423c0f027d3f98632e50098f6ca8141
Popo <candice@eolee.fr>
2026-02-02 13:52:26 +0100

    [ADD] Player Footsteps

--- Assets/Scripts/Audio/AudioPlayerAnim.cs
+++ Assets/Scripts/Audio/AudioPlayerAnim.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class AudioPlayerAniù : MonoBehaviour
 {
-    [SerializeField] EventID PlayerWalk;
-    [SerializeField] EventID PlayerWalkStop;
-    [SerializeField] EventID PlayerBend;
-    [SerializeField] EventID PlayerInteract;
-    [SerializeField] EventID PlayerCloatUp;
-    [SerializeField] EventID PlayerCloatDown;
+    [SerializeField] EventID playerWalk;
+    [SerializeField] EventID playerWalkStop;
+    [SerializeField] EventID playerBend;
+    [SerializeField] EventID playerInteract;
+    [SerializeField] EventID playerCloatUp;
+    [SerializeField] EventID playerCloatDown;
+
+    public void PostPlayerWalk()
+    {
+        AudioManager.Instance.PlayOneShot2D(playerWalk);
+    }
+
+    public void PostPlayerCloatDown()
+    {
+        AudioManager.Instance.PlayOneShot2D(playerCloatDown);
+    }
+
+    public void PostPlayerCloatUp()
+    {
+        AudioManager.Instance.PlayOneShot2D(playerCloatUp);
+    }
 }
