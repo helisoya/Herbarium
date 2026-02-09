f2b5941deb88b1a916f880b08528d4cb295a7a46
Julien OUDOT <oudot.julien@laposte.net>
2026-02-09 15:01:43 +0100

    [MOD] Fixed Quests not pinning correctly

--- Assets/Scripts/Cutscenes/Graph/Nodes/SetVariableNode.cs
+++ Assets/Scripts/Cutscenes/Graph/Nodes/SetVariableNode.cs
@@ -45,7 +45,7 @@ public class SetVariableNode : HerbariumNode
         handler.SetVariable(variableID,newValue);
 
         Quest quest = handler.GetQuestFromVariable(variableID);
-        if(quest != null && handler.IsPinned(variableID))
+        if(quest != null && handler.IsPinned(quest.id))
         {
             if(newValue == -1 || newValue == 100)
             {
@@ -59,7 +59,6 @@ public class SetVariableNode : HerbariumNode
         }else if(quest != null && newValue == 0)
         {
             // Quest not pinned and started
-
             handler.PinQuest(quest.id);
             GameGUI.instance.AddPin(quest.id);
         }
