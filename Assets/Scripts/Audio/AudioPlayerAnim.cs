67df0a175db5d560120eb77e38621a429fa1bf1d
Popo <candice@eolee.fr>
2026-02-13 09:51:35 +0100

    Revert "[MOD] Opti Footsteps"
    This reverts commit ee87f6f7f372d43ca2a6b150875f3181a1a75948.

--- Assets/Scripts/Audio/AudioPlayerAnim.cs
+++ Assets/Scripts/Audio/AudioPlayerAnim.cs
@@ -1,42 +1,26 @@
 using UnityEngine;
-using FMODUnity;
-using FMOD.Studio;
-using STOP_MODE = FMOD.Studio.STOP_MODE;
 
 public class AudioPlayerAniù : MonoBehaviour
 {
-    [SerializeField] private EventReference playerWalk;
+    [SerializeField] EventID playerWalk;
+    //[SerializeField] EventID playerWalkStop;
     //[SerializeField] EventID playerBend;
     //[SerializeField] EventID playerInteract;
-    [SerializeField] EventReference playerCloatUp;
-    [SerializeField] EventReference playerCloatDown;
-    
-    private EventInstance playerWalkInstance;
-    private EventInstance playerCloatUpInstance;
-    private EventInstance playerCloatDownInstance;
+    [SerializeField] EventID playerCloatUp;
+    [SerializeField] EventID playerCloatDown;
 
     public void PostPlayerWalk()
     {
-        playerWalkInstance = RuntimeManager.CreateInstance(playerWalk);
-        playerWalkInstance.start();
+        AudioManager.Instance.PlayOneShot2D(playerWalk);
     }
 
     public void PostPlayerCloatDown()
     {
-        playerCloatDownInstance = RuntimeManager.CreateInstance(playerCloatDown);
-        playerCloatDownInstance.start();
+        AudioManager.Instance.PlayOneShot2D(playerCloatDown);
     }
 
     public void PostPlayerCloatUp()
     {
-        playerCloatUpInstance = RuntimeManager.CreateInstance(playerCloatUp);
-        playerCloatUpInstance.start();
-    }
-
-    public void PostPlayerIdle()
-    {
-        playerWalkInstance.stop(STOP_MODE.IMMEDIATE);
-        playerCloatUpInstance.stop(STOP_MODE.IMMEDIATE);
-        playerCloatDownInstance.stop(STOP_MODE.IMMEDIATE);
+        AudioManager.Instance.PlayOneShot2D(playerCloatUp);
     }
 }
